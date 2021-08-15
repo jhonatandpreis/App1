df7a8c9d76b70162fd28ff7847868219f8ec4292
jhonatandpreis <jhonatandpreis@gmail.com>
2021-08-15 16:42:44 -0300

    Agenda 1.1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends  StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() {
-    return  HomePageState();
-}
-
+    return HomePageState();
+  }
 }
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-  return Container(
-    child: Center(
-      child: GestureDetector(
-        child:Text('Click $counter'),
-        onTap:(){
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        'Agenda Pessoal',
+      )),
+      body: Center(
+        child: GestureDetector(
+          child: Text('click aqui $counter', style:TextStyle(fontSize: 50),),
+          onTap: () {
+            setState(() {
+              counter--;
+              // print(counter);
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_sharp),
+        onPressed:(){
           setState(() {
-            counter ++;
-            print(counter);
-            
+            counter++;
           });
+
         } ,
       ),
-    ),
-
-
-  );  
-  
+    );
   }
-
 }
